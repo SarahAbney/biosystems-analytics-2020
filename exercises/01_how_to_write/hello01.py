4de0858c562823a63702164ec614c8a01df0caba
SarahAbney <seabney@email.arizona.edu>
2020-01-28 10:10:52 -0700

    stuff

--- exercises/01_how_to_write/hello01.py
+++ exercises/01_how_to_write/hello01.py
@@ -1,1 +1,12 @@
-print('Hello, World!')
+ #! usr/bin/env
+ #purpose: say hello
+
+import sys 
+
+args= sys.argv[1:]
+if args:
+    name = args [0]
+    print(f'Hello, {name}!')
+else:
+    print('Hello, World!')
+
