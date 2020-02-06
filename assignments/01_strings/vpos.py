2a4c19215e3018e5574cfe665fe5f3a4c0994db3
SarahAbney <seabney@email.arizona.edu>
2020-02-05 20:57:58 -0700

    homework.grademe

--- assignments/01_strings/vpos.py
+++ assignments/01_strings/vpos.py
@@ -1,48 +1,50 @@
 #!/usr/bin/env python3
-""" Author: Sarah Abney 
-    Purpose: Homework 1 """
+"""
+Author : Sarah Abney
+Date   : 5 Feburary 2020
+Purpose: Homework 1 
+"""
 
+import argparse
+import os
+import sys
 
-
-import argparse 
-import sys 
-import os 
 
 
 # --------------------------------------------------
 def get_args():
+    """Get arguments"""
+
+    parser = argparse.ArgumentParser(
+        description="Homework1",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('vowel', 
+                        metavar='vowel',
+                        help='vowel = A vowel to search for', 
+                        type=str, 
+                        choices= ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+
+    parser.add_argument('text',
+                        metavar='text',
+                        help='text= The text to show')
+
+    return parser.parse_args()
 
 
-            parser = argparse.ArgumentParser(
-                 description="Find positon of vowel in string",
-                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-            
-            parser.add_argument('vowel',
-                    metavar = 'vowel', 
-                    type=str,
-                    help='vowel = A vowel to search for')
-
-            parser.add_argument('text',  
-                    metavar='word',
-                    type=str,
-                    help= 'text = The text to show')
-            
-            return parser.parse_args() 
-
-#-----------------------------------------
-
-def main(): 
+# --------------------------------------------------
+def main():
+    """The fun stuff"""
 
     args = get_args()
-    text = args.text 
-    vowel = 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'
-    index = vowel.index('a','e','i')
-          = vowel.index('o', 'u', 'A')
-          = vowel.index('E', 'I', 'O')
-          = vowel.index('U')
-
-    print(f'Found "{vowel}" in "{text}" at index {index}') if text in 'aeiou' else print (f'"{vowel}" is not found in "{text}"') 
-#--------------------------------------------
-if __name__=='__main__':
+    text = args.text
+    vowel = args.vowel
+    
+    if vowel in text:
+        print(f'Found "{vowel}" in "{text}" at index {text.index(vowel)}.')
+    else: 
+        print(f'"{vowel}" is not found in "{text}".')
+   
+# --------------------------------------------------
+if __name__ == '__main__':
     main()
-
