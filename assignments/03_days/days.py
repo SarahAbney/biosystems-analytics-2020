afc193f0140170e32c43fd6acc153eeb849a5f04
SarahAbney <seabney@email.arizona.edu>
2020-02-19 10:15:59 -0700

    Finished3

--- assignments/03_days/days.py
+++ assignments/03_days/days.py
@@ -18,7 +18,7 @@ def get_args():
         description='What to do on each day',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('text',
+    parser.add_argument('day',
                         metavar='str',
                         nargs='+',
                         help='Days of the week')
@@ -31,21 +31,15 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    text = args.text
-    days = {"Monday": "On Mondays I never go to work",
+    todo = {"Monday": "On Mondays I never go to work",
             "Tuesday": "On Tuesdays I stay at home",
             "Wednesday": "On Wednesdays I never feel inclined",
-            "Thursday": "On Thursdays it's a holiday",
+            "Thursday": "On Thursdays, it's a holiday",
             "Friday": "And Fridays I detest",
             "Saturday": "Oh, it's much too late on a Saturday",
             "Sunday": "And Sunday is the day of rest"} 
-
-    if days in text: 
-        print(args.text.translate(text.maketrans(days)))
-
-    else:
-       print(f"Can't find {}") 
-
+    for day in args.day: 
+        print(todo.get(day, f"Can't find \"{day}\""))
 
 # --------------------------------------------------
 if __name__ == '__main__':
