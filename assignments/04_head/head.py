eb03bc6b38bf3f8bb7fc03468129cd6fff89ae4c
SarahAbney <seabney@email.arizona.edu>
2020-02-26 21:03:01 -0700

    finished04

--- assignments/04_head/head.py
+++ assignments/04_head/head.py
@@ -7,6 +7,7 @@ Purpose: Head Banger
 
 import argparse
 import os
+import io
 import sys
 
 
@@ -18,33 +19,34 @@ def get_args():
         description='Rock the Casbah',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('file',
-                        metavar='FILE',
-                        type=argparse.FileType('r'),
-                        help='Input File',
-                        default=None)
 
     parser.add_argument('-n',
                         '--num',
-                        nargs='+',
                         help='Number of Lines',
-                        metavar='str',
-                        type=str,
+                        metavar='int',
+                        type=int,
                         default=10)
+    
+    parser.add_argument('file',                                                                          metavar='FILE',                                                                  help='Input File',) 
 
     args = parser.parse_args()
-        
-    return args
 
+    if not args.num > 0:
+        parser.error(f'--num "{args.num}" must be greater than 0')
+
+    return args 
 
 # --------------------------------------------------
 def main():
     """Tell me what I want to hear"""
 
     args = get_args()
-    
-    
-    print(open(args.file).read.rstrip()[0:args.num]) 
+
+    for i in range(args.num): 
+        with open(args.file) as inputfile:
+            header = inputfile.read().splitlines()[i]    
+        print(f'{header}') 
+
 
 # --------------------------------------------------
 if __name__ == '__main__':
