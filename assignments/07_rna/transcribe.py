ea3a0b6841bec3041e9deccb97cd1d6ce63a1ea2
SarahAbney <seabney@email.arizona.edu>
2020-03-25 22:36:10 -0700

    transcribe.py

--- assignments/07_rna/transcribe.py
+++ assignments/07_rna/transcribe.py
@@ -2,7 +2,7 @@
 """
 Author : seabney
 Date   : 2020-03-25
-Purpose: Transcirbing and Jiving
+Purpose: DNR
 """
 
 import argparse
@@ -18,21 +18,20 @@ def get_args():
         description='Transcribing DNA into RNA',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument(
-        'file',
-        metavar='FILE',
-        nargs='*',
-        type=argparse.FileType('r'),
-        help='Input file(s)')
+    parser.add_argument('file',
+                        metavar='FILE',
+                        nargs='+',
+                        type=argparse.FileType('r'),
+                        help='Input file(s)')
 
-    parser.add_argument(
-        '-o',
-        '--outdir',
-        help='Output directory',
-        metavar='DIR',
-        type=str,
-        default='out')
+    parser.add_argument('-o',
+                        '--outdir',
+                        help='Output directory',
+                        metavar='DIR',
+                        type=str,
+                        default='out')
 
+    
     return parser.parse_args()
 
 
@@ -41,29 +40,44 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    dna = args.file
-    out = args.outdir
-    total_lines = 0
-    
-    for fh in dna:
-        lines = 0
-        out_file = os.path.join(out_dir, os.path.basename(fh.name)
-        out_fh = open(out_file, 'wt')
-        
-        total_files = len(out)
+    out_dir = args.outdir
+    total_seq = 0
+    total_files = 0
 
+#    more = 's' if total_files > 1 else ''
+
+    
+    for fh in args.file:
+        lines = 0
+        out_file = os.path.join(out_dir, os.path.basename(fh.name))
+        if not os.path.isdir(out_dir):
+            os.makedirs(out_dir)
+        out_fh = open(out_file, 'wt')
+    if os.path.isfile(out_file):
+        out_file = open(out_file).read().rstrip() 
+
+        more = 's' if total_files > 1 else ''
+        total_files = len(os.listdir(out_dir))
+
+ 
         for line in fh:
             lines += 1
-            rna = ['U' if char == 'T' else char for char in dna]
+            rna = ['U' if char == 'T' else char for char in line] 
+            listToStr = ''.join(map(str, rna))
+            plural = 's' if total_seq > 1 else ''
+        total_seq += lines
+        plural = 's' if total_seq > 1 else ''
 
-        total_lines += lines
 
-        out_fh.write(rna + '\n')
+        out_fh.write(listToStr)
+        out_fh.close()
+    
 
-        print(
-            'Done, wrote {total_lines} sequences in {total_files} file into directory "out".'
-        )
-
+        
+         
+    print(f'Done, wrote {total_seq} sequence{plural} in {total_files} file{more} to directory "{out_dir}".')
+#    else: 
+#        print(f'Done, wrote {total_seq} sequence in {total_files} files to directory "out".')
 
 # --------------------------------------------------
 if __name__ == '__main__':
