3232bf56d34dc3376a248b5ffef089f2568b166c
SarahAbney <seabney@email.arizona.edu>
2020-03-31 20:27:05 -0700

    dont get the test...

--- assignments/08_synthetic_dna/moog.py
+++ assignments/08_synthetic_dna/moog.py
@@ -23,7 +23,7 @@ def get_args():
                         '--outfile',
                         metavar='str',
                         help='Output filename',
-                        type=argparse.FileType('wt'),
+                        type=str,
                         default='out.fa')
 
     parser.add_argument('-t',
@@ -31,6 +31,7 @@ def get_args():
                         help='DNA or RNA',
                         metavar='str',
                         type=str,
+                        choices=['rna', 'dna'],
                         default='dna')
 
     parser.add_argument('-n',
@@ -71,6 +72,7 @@ def get_args():
 
     if not 0 < args.pctgc < 1:
         parser.error(f'--pctgc "{args.pctgc}" must be between 0 and 1')
+    
 
     return args
 
@@ -82,16 +84,19 @@ def main():
     args = get_args()
     random.seed(args.seed)
     pool = create_pool(args.pctgc, args.maxlen, args.seqtype)
+    out_fh = open(args.outfile, 'wt')
 
-    print(pool)
 
+    i_total = 0
+    for i in range(args.numseqs):
+        seq_len = random.randint(args.minlen, args.maxlen)
+        seq = ''.join(random.sample(pool, k=seq_len)).upper()
+        i_total += 1 
+        out_fh.write(f'>{i_total} \n{seq} \n')
+    
+    out_fh.close()
 
-#    for i in range(args.numseqs):
-#        seq_len = random.randint(args.minlen, args.maxlen)
-#        seq = ''.join(random.sample(pool, k=seq_len))
-#        args.outfile.write(f'> \n {seq}')
-
-#    print(f'Done, wrote {args.numseq} {args.seqtype} sequences to "{args.outfile}".')
+    print(f'Done, wrote {args.numseqs} {args.seqtype} sequences to "{args.outfile}".')
 
 
 # --------------------------------------------------
